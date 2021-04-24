31521daa3774dc04616ff3ec421e73e85702afa7
ETavaresLCIDemo <etavareslcidemo@gmail.com>
2021-04-24 08:38:54 -0700

    Assignment 1 marked.

--- VGP220_Assignments/VGP220_Assignment1_FranciscoCastillo/main.cpp
+++ VGP220_Assignments/VGP220_Assignment1_FranciscoCastillo/main.cpp
@@ -3,6 +3,7 @@
 
 //Name: Juan Francisco Castillo Regalado
 //StudentId: 2042805
+//Grade: 92/100
 
 int RecursiveMultiplication(int a, int b);
 int NonRecursiveFibonacci(int n);
@@ -15,17 +16,35 @@ int main(int argc, char* argv[])
 	//Tests:
 	std::cout << RecursiveMultiplication(2, 5) << std::endl; // Expected result: 10
 	
-	std::cout << NonRecursiveFibonacci(5) << std::endl; //Expected result: 5
+	std::cout << NonRecursiveFibonacci(8) << std::endl; //Expected result: 5
 	
-	PrintFibonacciLessThan15(7); //Expected result: 0 1 1 2 3 5 8 13
+	PrintFibonacciLessThan15(14); //Expected result: 0 1 1 2 3 5 8 13
 	
 	std::cout << std::endl;// ->Adding an extra line
 
-	printRecursiveFactorialLessThan50(10);//Expected result: 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+	printRecursiveFactorialLessThan50(49);//Expected result: 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 
 	return 0;
 }
-
+//ERROR: -8. You don't have to assert that a and b are positive or non zero. 
+//Multiplications by zero or by negative numbers they exist and should be considered.
+//You also have to treat when b is negative.
+//A good answer for this question is:
+//int RecursiveMultiplication(int a, int b)
+//{
+//	if (b == 0) 
+//	{
+//		return 0;
+//	}
+//	if (b > 0) 
+//	{
+//		return a + RecursiveMultiplication(a, b - 1);
+//	}
+//	if (b < 0) 
+//	{
+//		return -RecursiveMultiplication(a, -b);
+//	}
+//}
 int RecursiveMultiplication(int a, int b)
 {
 	//TODO: Write a recursive function to multiply two positive integers
@@ -52,6 +71,7 @@ int RecursiveMultiplication(int a, int b)
 	}
 }
 
+//COMMENT: NICE!
 int NonRecursiveFibonacci(int n)
 {
 	//TODO: Convert the recursive fibonacci that we did in class
@@ -85,6 +105,7 @@ int NonRecursiveFibonacci(int n)
 	return 0;
 }
 
+//COMMENT: GOOD!
 int PrintFibonacciLessThan15(int n)
 {
 	//TODO: Create a method that will print all the fibonacci sequence
@@ -123,6 +144,7 @@ int PrintFibonacciLessThan15(int n)
 	return 0;
 }
 
+//COMMENT: GOOD!
 int printRecursiveFactorialLessThan50(int n)
 {
 	//TODO: Create a method that will print recursive
