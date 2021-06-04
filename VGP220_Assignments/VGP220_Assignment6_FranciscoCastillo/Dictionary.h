3b874d68ee61d035f1db3e089168986a8a46d6f5
frank-castillo <juanfracocastillo@hotmail.com>
2021-06-04 01:44:45 -0700

    Upload assignment 06 - Hash Table

--- VGP220_Assignments/VGP220_Assignment6_FranciscoCastillo/Dictionary.h
+++ VGP220_Assignments/VGP220_Assignment6_FranciscoCastillo/Dictionary.h
@@ -32,6 +32,23 @@ public:
 		//  If you choose II, nested for loop to initialize each key as empty:
 		//        mData[i][j].key = "";
 		//This initialization is required that you can check if the position for insertion is available or not
+
+		mData = new Data<T>*[capacity];
+
+		for (int i = 0; i < capacity; ++i)
+		{
+			mData[i] = new Data<T>*[5];
+		}
+
+		for (int i = 0; i < capacity; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+				mData[i][j] = nullptr;
+			}
+		}
+
+		mCapacity = capacity * 5;
 	}
 
 	~Dictionary()
@@ -39,6 +56,13 @@ public:
 		//Task 2
 		// Implement the destructor for this class.
 		// Don't forget that if you "new" a data, you have to "delete" this data;
+
+		for (int i = 0; i < capacity; ++i)
+		{
+			delete[] mData[i];
+		}
+
+		delete[] mData;
 	}
 
 	//Task 3 - This hash function receives a key and transforms this key into a int that
@@ -57,18 +81,33 @@ public:
 
 	// If you choose II, one of the variables that you can use as parameter is the seed.
 	//  example: int Hash(const std::string& key, int seed)
-	int Hash(const std::string& key)
+	int Hash(const std::string& key, int newSeed)
 	{
-		int seed = 131;
-		int hash = 0;
-		for (int i = 0; i < key.length(); ++i)
+		if (newSeed == 0)
 		{
-			//You don't need to cast, since hash is an integer and you are adding
-			//a character, the cast is implicit. static_cast<int> in this case is ambiguous.
-			hash += key[i];
+			int seed = 131;
+			int hash = 0;
+			for (int i = 0; i < key.length(); ++i)
+			{
+				//You don't need to cast, since hash is an integer and you are adding
+				//a character, the cast is implicit. static_cast<int> in this case is ambiguous.
+				hash += key[i];
+			}
+			hash *= seed;
+			return hash % (mCapacity / 5);
 		}
-		hash *= seed;
-		return hash % mCapacity;
+		else
+		{
+			int secondSeed = newSeed;
+			int secondHash = 0;
+			for (int i = 0; i < key.length(); ++i)
+			{
+				secondHash += key[i];
+			}
+			secondHash *= seed;
+			return secondHash % 5;
+		}
+		
 	}
 
 	void Insert(const std::string& key, T value)
@@ -76,7 +115,45 @@ public:
 		//Task 4: This is the hardest method to be implemented. You will need to hash 2 times.
 		// Make sure to find two indices (Using your hash functions) and insert to the next available position. If you don't 
 		//	have an available position, print a message to the user saying:
-		// "Element cannot be inserted. Bucket at index %s is full." %s = hashIndex. 
+		// "Element cannot be inserted. Bucket at index %s is full." %s = hashIndex.
+
+		if (key.empty())
+		{
+			std::cout << "This map cannot store empty keys." << std::endl;
+			return;
+		}
+
+		if (mCurrentSize == mCapacity)
+		{
+			std::cout << "This map is full." << std::endl;
+			return;
+		}
+
+		// Check for similar keys before inserting
+		for (int i = 0; i < capacity; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+				if (mData[i][j].key.compare(key) == 0)
+				{
+					std::cout << "This key is already registered. Please use a different key." << std::endl;
+					return;
+				}
+			}
+		}
+
+		int firstHashArrayIndex = Hash(key, 0);
+		int secondHashArrayIndex = Hash(key, 234);
+
+		if (mData[firstHashArrayIndex][secondHashArrayIndex].key.empty())
+		{
+			mData[firstHashArrayIndex][secondHashArrayIndex].key = key;
+			mData[firstHashArrayIndex][secondHashArrayIndex].value = value;
+			++mCurrentSize;
+			return;
+		}
+
+		std::cout << "Element cannot be inserted. Bucket at index " << firstHashArrayIndex << " is full." << std::endl;
 	}
 
 private:
